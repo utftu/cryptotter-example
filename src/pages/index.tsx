5a9b7e89134c9745b6cefe8b28822c30c9456c3a
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-05-20 00:53:54 +0300

    20.05.22 00:53

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,38 +3,59 @@ import {useFfw, useInitFfw} from 'ffw';
 import {Input} from 'baseui/input';
 import {CryptotterButton} from 'cryptotter-react/dist/esm/dev.js';
 import {createTransaction} from '../connections';
+import {Select} from 'baseui/select';
+
+const typeOptions = [
+  {label: 'Popup', value: 'popup'},
+  {label: 'Tab', value: 'tab'},
+];
 
 const Home: NextPage = () => {
   const ffw = useInitFfw({
     initValues: {
       count: '1',
+      type: 'popup',
     },
   });
   useFfw({
     form: ffw,
   });
+  globalThis.form = ffw;
 
   return (
     <div className={'grow flex justify-center items-center'}>
-      <div className={'grow mt-4 flex flex-col max-w-xs'}>
+      <div className={'grow flex flex-col max-w-xs'}>
         <img
+          className={'self-center'}
+          width={100}
           src={
-            'https://upload.wikimedia.org/wikipedia/ru/e/e5/Magritte_TheSonOfMan.jpg'
+            'https://berezovski.by/wp-content/uploads/2018/07/Test-Logo-Circle-black-transparent.png'
           }
         />
-        <div className={'mt-4 self-center'}>Virtual apple for test</div>
-        <div className={'mt-4 grid grid-cols-2 gap-3'}>
+        <div className={'mt-8 self-center'}>Virtual product for test</div>
+        <div className={'mt-8 grid grid-cols-2 gap-8'}>
           <div>Price:</div>
           <div>0.001$</div>
           <div>Count:</div>
           <Input {...ffw.f.count.getInput()} type={'number'} />
           <div>Total price:</div>
           <div>{ffw.f.count.value * 0.001}$</div>
+          <div>Type of payment page</div>
+          <Select
+            options={typeOptions}
+            valueKey={'value'}
+            value={[typeOptions.find(({value}) => value === ffw.f.type.value)]}
+            clearable={false}
+            onChange={(params) => {
+              ffw.f.type.set(params.option.value);
+            }}
+          />
         </div>
         <CryptotterButton
           className={'mt-8'}
           onSuccess={() => {}}
-          type={'popup'}
+          type={ffw.f.type.value}
+          payOrigin={process.env['NEXT_PUBLIC_PAY']}
           onClick={async () => {
             const transaction = await createTransaction({
               amount: 0.01 * ffw.f.count.value,
